f16905999997be30c71e31d4103a26b35deb6f2f
Samuel Kime <samuelkime7@gmail.com>
2022-12-06 05:51:27 +0100

    changed date wahala

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
-const year = new Date().getFullYear();
+import Moment from 'moment';
 
 const Footer = () => {
   return (
     <footer>
       <p className="name">Created by Samuel Kime</p>
-      <p className="copyright">&copy; {year}</p>
+      <p className="copyright">&copy; {Moment().year()}</p>
       <p className="api">
         Used <a href="https://www.themoviedb.org/">The Movie Database</a> API.
       </p>
