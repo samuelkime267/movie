0a64c160061fb98db21a707b62956d70370fcaad
Samuel Kime <samuelkime7@gmail.com>
2023-01-01 23:04:28 +0100

    loader fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 import '../scss/main.scss';
 import { Navbar, Footer, Loader } from '../components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState();
   const router = useRouter();
-  router.events.on('routeChangeStart', () => setIsLoading(true));
-  router.events.on('routeChangeComplete', () => setIsLoading(false));
-  router.events.on('routeChangeError', () => setIsLoading(false));
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => setIsLoading(true));
+    router.events.on('routeChangeComplete', () => setIsLoading(false));
+    router.events.on('routeChangeError', () => setIsLoading(false));
+  });
   return (
     <>
       {isLoading && <Loader />}
